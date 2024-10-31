8535ac43d19e6e96d869fc6e6c78a716bd895aac
nafimahbubucsd <130004889+nafimahbubucsd@users.noreply.github.com>
2024-10-31 00:27:43 -0700

    Update App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,22 +1,28 @@
+// src/App.tsx
 import React from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
-import { Route, Routes, Link } from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom";
 import { routes } from '../constants/routes';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-            <Link to="/">Home</Link> <Link to="/test">Test</Link>
-        </nav>
-        <Routes>
-            {/* Insert your Route Here */}
-            {routes.map((route) => (<Route path={route.path} element={route.element}/>))}
-        </Routes>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <nav>
+              <Link to="/">Home</Link> <Link to="/test">Test</Link>
+          </nav>
+          <Routes>
+              {routes.map((route) => (
+                <Route key={route.path} path={route.path} element={route.element} />
+              ))}
+          </Routes>
+        </header>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
