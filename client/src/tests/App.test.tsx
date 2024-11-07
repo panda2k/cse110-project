34c5764629197b232d43c8a5bf5d5d9114ca0ab5
alw017 <alw017@ucsd.edu>
2024-11-06 21:36:41 -0800

    added vacuous initial test and fixed incorrect directory in config

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,3 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
+
+test('vacuous test', () => {
+	render(<BrowserRouter><React.StrictMode><App/></React.StrictMode></BrowserRouter>);
+	const notInDocument = screen.queryByText("$!#@)");
+	expect(notInDocument).toBeNull();
+});
+
