da6f437aac86157b9f05c5a7477a9480f57d540a
alw017 <alw017@ucsd.edu>
2024-11-11 13:43:13 -0800

    fixed function name typo and added default backend url to constants

--- client/src/utils/event-utils.ts
+++ client/src/utils/event-utils.ts
@@ -2,7 +2,7 @@ import { API_BASE_URL } from "../constants/misc";
 import { ClubEvent } from "../types/Event";
 import { User } from "../types/User";
 
-export const getRVSP = async (event: ClubEvent): Promise<User[]> => { 
+export const getRSVP = async (event: ClubEvent): Promise<User[]> => { 
 	const response = await fetch(`${API_BASE_URL}/rsvp/${event.id}`	);
 	if (!response.ok) {
     	throw new Error("Failed to get RSVPs");
