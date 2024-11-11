18d9a06d714fde7c15702c90c6c51f165da7c6e8
pabloooA <146864378+Pedroo-A@users.noreply.github.com>
2024-11-11 15:40:57 -0800

    Front end baseline, connect backend

---
+++ client/src/constants/EventLists.ts
@@ -0,0 +1,35 @@
+export type Post = {
+	id: number; 
+	title: string;
+	description: string;
+};
+
+export const Events: Post [] = [
+	{
+    id: 1,
+	title: "TEST EVENT",
+	description: "Just testing out how these things will look when applying them and all that such ."
+},
+
+{
+    id: 2,
+	title: "TEST EVENT 2",
+	description: "Just testing out how these things will look when applying them and all that such LEDLE LEDLE LEDLE."
+},
+{
+    id: 3,
+	title: "Pedro's Extreme bonnanza",
+	description: "Just testing out how these things will look when applying them and all that such LEDLE LEDLE LEDLE. uhhh baasically this event will entail something stupid and dumb and dumb and stupid. I also just want to see how long I can make this thing so AAHHHHHHHHHHHHHHHHHHHHHHHHHHHHH HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+},
+{
+    id: 4,
+	title: "This one will test the lengths I can make for descriptions and the titles",
+	description: "I should probably put a word limit on these but idk what exactly just yet or maybe a word/character limit on just the title. womp womp uhhhhhhhh I mean basically as long as no one really just fills up the text spaces with a really long and unbroken word it should be fine but ill still fix it so that it desiplays properly. I also just want to test out to see if different posts will have different lengths and such, not currently working right now. MAAAAN"
+
+},
+{
+    id: 5,
+	title: "TEST EVENT 5",
+	description: "This one just testing what happen when we reach max posts in a row"
+}
+]
