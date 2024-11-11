e1cc5beb21cb79b84257b9949850aeecb15ca790
pabloooA <146864378+Pedroo-A@users.noreply.github.com>
2024-11-11 15:17:29 -0800

    Created front end, just need to connect backend.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,13 +3,16 @@ import logo from '../assets/logo.svg';
 import '../styles/App.css';
 import { Route, Routes, Link } from "react-router-dom"
 import { routes } from '../constants/routes';
+import { NewEvents } from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <nav>
-            <Link to="/">Home</Link> <Link to="/test">Test</Link>
+            <Link to="/">Home</Link> 
+            <Link to="/test">Test</Link> 
+            <Link to= "/home">New Events</Link> {/*Link to Students view of events*/ } 
         </nav>
         <Routes>
             {/* To add a view to the website, go to the routes.tsx file in the constants directory, and add an entry to the array defined in the file.*/}
