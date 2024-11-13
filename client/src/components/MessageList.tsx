14011b0ec977797365e8a9dd9bd2cacca1f3f55c
panda2k <thelowkeypanda@gmail.com>
2024-11-12 20:53:40 -0800

    add component refreshing

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { Conversation } from "../types/types";
+import { getMessages } from "../utils";
+
 export default function MessageList() {
+    const [messages, setMessages] = useState<Conversation[]>([]);
+
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            setMessages(await getMessages());
+        }, 15000)
+        return () => clearInterval(interval);
+    }, [setMessages]);
 
     return (
         <div>
