6f7a777d1eacabf8ce515fb1e9d941b707dbba3f
alw017 <alw017@ucsd.edu>
2024-11-23 15:36:49 -0800

    copied code from app.ts into index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import 'dotenv/config';
 import { userRoutes } from "./routes/users";
+import authRoutes from './routes/auth';
+import cors from 'cors';
 //import { eventRoutes } from "./routes/events";
 
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-var server = app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+app.use(cors());
+app.use(express.json());
+
+var server = app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
 
 app.use("/users", userRoutes);
-//app.use("/events", eventRoutes);
+app.use('/api/auth', authRoutes);
+
 
 module.exports = { 
     app: app,
