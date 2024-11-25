85684f23b6a6c65b0a3d6730c279c800d92e3de4
Michael Wang <thelowkeypanda@gmail.com>
2024-11-24 18:35:37 -0800

    fix styling

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -149,10 +149,12 @@ export default function MessageList() {
                             }
                         </div>
                         <form className="send-message" onSubmit={onFormSubmit}>
-                            <input id="content" type="text" placeholder="Message here..." />
-                            <button type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-send-horizontal"><path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z" /><path d="M6 12h16" /></svg>
-                            </button>
+                            <div>
+                                <input id="content" type="text" placeholder="Message here..." />
+                                <button type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-send-horizontal"><path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z" /><path d="M6 12h16" /></svg>
+                                </button>
+                            </div>
                         </form>
                     </>
                 ) : <div className="info-message">Select a conversation first</div>}
