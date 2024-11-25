a00c34a098e706c6b2d8a9d8afe1e625288bbd85
risabsank <risab.sankar@gmail.com>
2024-11-25 12:49:59 -0800

    Added revert

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,31 +1,9 @@
-// src/App.tsx
 import React from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
-import { Route, Routes, Link } from "react-router-dom";
-import { routes } from '../constants/routes';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthProvider } from '../context/AuthContext';
+import UserHomepage from './student_user/UserHomepage';
+import ClubPage from './student_org/ClubPage';
 
 function App() {
-  return (
-    <GoogleOAuthProvider clientId="409242628246-5miauio66oad9ltrr3lb17gbb0r9m04i.apps.googleusercontent.com">
-      <AuthProvider>
-        <div className="App">
-          <header className="App-header">
-            <nav>
-              <Link to="/">Home</Link> <Link to="/test">Test</Link>
-            </nav>
-          </header>
-          <Routes>
-            {routes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </div>
-      </AuthProvider>
-    </GoogleOAuthProvider>
-  );
+  return <ClubPage />;
 }
 
 export default App;
